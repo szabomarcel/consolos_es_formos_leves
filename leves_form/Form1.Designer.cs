153b8d76576c86bb12682f5ef431c550eecbafc4
szabomarcel <szabomarcell34@gmail.com>
2024-04-18 10:38:19 +0200

    feladat

---
+++ leves_form/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace leves_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_mentes = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_megnevezes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_kaloria = new System.Windows.Forms.TextBox();
+            this.textBox_feherje = new System.Windows.Forms.TextBox();
+            this.textBox_zsir = new System.Windows.Forms.TextBox();
+            this.textBox_szenhidrat = new System.Windows.Forms.TextBox();
+            this.textBox_hamu = new System.Windows.Forms.TextBox();
+            this.textBox_rost = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_mentes
+            // 
+            this.button_mentes.Location = new System.Drawing.Point(36, 265);
+            this.button_mentes.Name = "button_mentes";
+            this.button_mentes.Size = new System.Drawing.Size(200, 23);
+            this.button_mentes.TabIndex = 0;
+            this.button_mentes.Text = "Mentés";
+            this.button_mentes.UseVisualStyleBackColor = true;
+            this.button_mentes.Click += new System.EventHandler(this.button_mentes_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Megnevezés:";
+            // 
+            // textBox_megnevezes
+            // 
+            this.textBox_megnevezes.Location = new System.Drawing.Point(36, 58);
+            this.textBox_megnevezes.Name = "textBox_megnevezes";
+            this.textBox_megnevezes.Size = new System.Drawing.Size(200, 20);
+            this.textBox_megnevezes.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kalória:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fehérje:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Zsír:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Szénhidrát:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Hamu:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(116, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Rost:";
+            // 
+            // textBox_kaloria
+            // 
+            this.textBox_kaloria.Location = new System.Drawing.Point(6, 32);
+            this.textBox_kaloria.Name = "textBox_kaloria";
+            this.textBox_kaloria.Size = new System.Drawing.Size(71, 20);
+            this.textBox_kaloria.TabIndex = 9;
+            // 
+            // textBox_feherje
+            // 
+            this.textBox_feherje.Location = new System.Drawing.Point(110, 32);
+            this.textBox_feherje.Name = "textBox_feherje";
+            this.textBox_feherje.Size = new System.Drawing.Size(72, 20);
+            this.textBox_feherje.TabIndex = 10;
+            // 
+            // textBox_zsir
+            // 
+            this.textBox_zsir.Location = new System.Drawing.Point(6, 82);
+            this.textBox_zsir.Name = "textBox_zsir";
+            this.textBox_zsir.Size = new System.Drawing.Size(71, 20);
+            this.textBox_zsir.TabIndex = 11;
+            // 
+            // textBox_szenhidrat
+            // 
+            this.textBox_szenhidrat.Location = new System.Drawing.Point(110, 82);
+            this.textBox_szenhidrat.Name = "textBox_szenhidrat";
+            this.textBox_szenhidrat.Size = new System.Drawing.Size(72, 20);
+            this.textBox_szenhidrat.TabIndex = 12;
+            // 
+            // textBox_hamu
+            // 
+            this.textBox_hamu.Location = new System.Drawing.Point(6, 137);
+            this.textBox_hamu.Name = "textBox_hamu";
+            this.textBox_hamu.Size = new System.Drawing.Size(71, 20);
+            this.textBox_hamu.TabIndex = 13;
+            // 
+            // textBox_rost
+            // 
+            this.textBox_rost.Location = new System.Drawing.Point(110, 137);
+            this.textBox_rost.Name = "textBox_rost";
+            this.textBox_rost.Size = new System.Drawing.Size(72, 20);
+            this.textBox_rost.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_rost);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_hamu);
+            this.groupBox1.Controls.Add(this.textBox_kaloria);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox_szenhidrat);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox_feherje);
+            this.groupBox1.Controls.Add(this.textBox_zsir);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(36, 84);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 175);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 334);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox_megnevezes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_mentes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Leves";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_mentes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_megnevezes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_kaloria;
+        private System.Windows.Forms.TextBox textBox_feherje;
+        private System.Windows.Forms.TextBox textBox_zsir;
+        private System.Windows.Forms.TextBox textBox_szenhidrat;
+        private System.Windows.Forms.TextBox textBox_hamu;
+        private System.Windows.Forms.TextBox textBox_rost;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ leves_form/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace leves_form
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_mentes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string soupName = textBox_megnevezes.Text;
+                string kaloria = textBox_kaloria.Text;
+                string feherje = textBox_feherje.Text;
+                string zsir = textBox_zsir.Text;
+                string szenhidrat = textBox_szenhidrat.Text;
+                string hamu = textBox_hamu.Text;
+                string rost = textBox_rost.Text;
+
+                string apiUrl = "http://localhost/_levesek_vizsgaszeru_/backendleves/index.php?leves";
+                var values = new Dictionary<string, string>
+                {
+                    { "soupName", soupName },
+                    { "kaloria", kaloria },
+                    { "feherje", feherje },
+                    { "zsir", zsir },
+                    { "szenhidrat", szenhidrat },
+                    { "hamu", hamu },
+                    { "rost", rost }
+                };
+                var content = new FormUrlEncodedContent(values);
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                MessageBox.Show("Leves hozzáadva: " + soupName + "\nAdatok lekérve sikeresen.", "Mentés", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(HttpRequestException ex)
+            {
+                MessageBox.Show("Hiba történt a kérés során: " + ex.Message, "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
